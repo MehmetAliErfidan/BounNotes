ba355b6ba8b1d34fe37242c54430a6a9ef87a4fd
Mehmet Ali Erfidan <erfidan740@gmail.com>
2025-11-25 23:36:55 +0300

    feat: create styled component for SearchInput and refactor to use it

---
+++ src/components/search/!SearchInput.styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  display: flex;
+  width: 100%;
+  max-width: 36rem; /* Tailwind’de max-w-xl */
+  align-items: stretch;
+`;
--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -1,4 +1,5 @@
 import SearchIcon from "./SearchIcon";
+import { Form } from "./!SearchInput.styled";
 
 interface SearchInputProps {
   value: string;
@@ -14,7 +15,7 @@ export default function SearchInput({
   placeholder = "",
 }: SearchInputProps) {
   return (
-    <form onSubmit={onSubmit} className="flex w-full max-w-xl items-stretch">
+    <Form onSubmit={onSubmit}>
       <input
         type="search"
         value={value}
@@ -28,6 +29,6 @@ export default function SearchInput({
       >
         <SearchIcon className="w-5 h-5 sm:w-6 sm:h-6" />
       </button>
-    </form>
+    </Form>
   );
 }
