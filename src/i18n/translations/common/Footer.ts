353eb7ae505272eddb98a2d89ef6cf7934c1806e
Mehmet Ali Erfidan <erfidan740@gmail.com>
2025-11-26 14:10:24 +0300

    feat: add translation files for Footer and Navbar components

---
+++ src/i18n/translations/common/Footer.ts
@@ -0,0 +1,20 @@
+export const FOOTER_TEXTS = {
+  tr: {
+    appDefinition:
+      "Boğaziçi Öğrencileri İçin Ders Notu Paylaşımını Kolaylaştıran Platform",
+    contactInfo: "İletişim:",
+    email: "E-posta Adresi: ",
+    about: "Hakkında",
+    FAQ: "SSS",
+    copyrightMessage: "© 2025 Tüm hakları saklıdır.",
+  },
+  en: {
+    appDefinition:
+      "A Platform That Makes Sharing Class Notes Easier for Boğaziçi Students",
+    contactInfo: "Contact:",
+    email: "E-mail Address: ",
+    about: "About",
+    FAQ: "FAQ",
+    copyrightMessage: "© 2025 All rights reserved.",
+  },
+};
---
+++ src/i18n/translations/common/Navbar.ts
@@ -0,0 +1,10 @@
+export const NAVBAR_TEXTS = {
+  tr: {
+    login: "Giriş Yapın",
+    register: "Kayıt Olun",
+  },
+  en: {
+    login: "Login",
+    register: "Register",
+  },
+};
