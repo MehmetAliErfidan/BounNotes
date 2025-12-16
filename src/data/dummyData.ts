fd3903f6a269a3cbbc0e9a1dc5d60ba01ff0a504
Mehmet Ali Erfidan <erfidan740@gmail.com>
2025-12-16 11:47:48 +0300

    feat: add dummy data for notes and implement Redux store with custom hooks for state management

---
+++ src/data/dummyData.ts
@@ -0,0 +1,37 @@
+import type { Note } from "../components/note/NoteTypes";
+
+export const dummyData: Note[] = [
+  {
+    id: 1,
+    title: "Matematik 1 Final Notları",
+    course: "Matematik",
+    teacher: "Ahmet",
+    username: "Ali",
+    rating: 4.8,
+    price: "₺10",
+    date: "2024",
+    description: "Bu notlar sınavda çıkan tüm önemli konuları içerir.",
+  },
+  {
+    id: 2,
+    title: "Fizik 2 Özet",
+    course: "Fizik",
+    teacher: "Mehmet",
+    username: "Ayşe",
+    rating: 4.3,
+    price: "₺15",
+    date: "2023",
+    description: "Kısa ama etkili bir özet.",
+  },
+  {
+    id: 3,
+    title: "Kimya Final Notları",
+    course: "Kimya",
+    teacher: "Samet",
+    username: "Veli",
+    rating: 4.7,
+    price: "₺45",
+    date: "2024",
+    description: "kimyadır...",
+  },
+];
