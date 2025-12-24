0a38b9e303582b89cd7670b3e14fd9748c80c772
Mehmet Ali Erfidan <erfidan740@gmail.com>
2025-12-24 11:23:48 +0300

    language switcher button got a new style

--- src/components/common/LanguageSwitcher.tsx
+++ src/components/common/LanguageSwitcher.tsx
@@ -6,9 +6,25 @@ export default function LanguageSwitcher() {
   return (
     <button
       onClick={() => setLang(lang === "tr" ? "en" : "tr")}
-      className="rounded-l border "
+      className="
+        flex items-center justify-center
+
+        rounded-full
+        border border-gray-300
+        bg-gray-100
+
+        text-gray-700 font-medium
+
+        transition-all duration-200
+        hover:bg-gray-200
+        cursor-pointer
+        px-3 py-1.5 text-xs
+        sm:px-3 sm:py-1.5 sm:text-xs
+        md:px-3.5 md:py-2 md:text-sm
+        lg:px-4 lg:py-2 lg:text-sm
+      "
     >
-      {lang === "tr" ? "English" : "Türkçe"}
+      {lang === "tr" ? "EN" : "TR"}
     </button>
   );
 }
