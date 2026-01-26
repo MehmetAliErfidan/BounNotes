c659b556217ac2c358d453a40119debf81ffb478
Mehmet Ali Erfidan <erfidan740@gmail.com>
2026-01-26 23:46:16 +0300

    feat: add new i18n texts for CreateNote to be used in NoteUpload

--- src/i18n/translations/notes/CreateNote.ts
+++ src/i18n/translations/notes/CreateNote.ts
@@ -1,4 +1,12 @@
 export const CREATE_NOTE_TEXTS = {
-  tr: { uploadText: "Yükle" },
-  en: { uploadText: "Upload" },
+  tr: {
+    uploadText: "Yükle",
+    headerText: "Notunu Paylaş",
+    headerExplanation: "Not bilgilerinizi girin ve dosyalarınızı yükleyin.",
+  },
+  en: {
+    uploadText: "Upload",
+    headerText: "Share Your Note",
+    headerExplanation: "Fill in the details and upload your files.",
+  },
 };
