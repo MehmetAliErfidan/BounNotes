3c482835574a83bde95258da914b8162ee8124ed
Mehmet Ali Erfidan <erfidan740@gmail.com>
2026-01-26 23:40:57 +0300

    refactor: clarify search form styled component name

--- src/components/search/!SearchInput.styled.ts
+++ src/components/search/!SearchInput.styled.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Form = styled.form`
+export const SearchForm = styled.form`
   display: flex;
   width: 100%;
   max-width: 36rem; /* Tailwind’de max-w-xl */
