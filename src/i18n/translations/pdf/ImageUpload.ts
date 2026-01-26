29aef2401971b999ebf8d77e17a19bdc06e27255
Mehmet Ali Erfidan <erfidan740@gmail.com>
2026-01-26 23:48:10 +0300

    feat: update and extend ImageUpload i18n texts

--- src/i18n/translations/pdf/ImageUpload.ts
+++ src/i18n/translations/pdf/ImageUpload.ts
@@ -1,12 +1,14 @@
 export const IMAGE_UPLOAD_TEXTS = {
   tr: {
     onlyImageFiles: "Sadece Görsel Dosyalar Yüklenebilir.",
-    previewTitleText: "Önizlemede Gösterilecek Görseller",
+    previewTitleText: "Önizleme Görselleri",
+    deleteImage: "Sil",
     minImagesError: "En Az {{min}} Görsel Eklemelisiniz",
   },
   en: {
     onlyImageFiles: "Only Image Files Can Be Uploaded.",
     previewTitleText: "Preview Images to Display",
+    deleteImage: "Delete",
     minImagesError: "Add at Least {{min}} Image",
   },
 };
