849617298e84fe198c7e592c411b77dda855cbe6
Mehmet Ali Erfidan <erfidan740@gmail.com>
2026-01-27 23:25:46 +0300

    feat(i18n): add profile summary translations

---
+++ src/i18n/translations/profile/user-profile/ProfileSummary.ts
@@ -0,0 +1,10 @@
+export const PROFILE_SUMMARY_TEXTS = {
+  tr: {
+    profileInformation: "Profil Bilgileri",
+    editProfile: "Profilini Düzenle",
+  },
+  en: {
+    profileInformation: "Profile Information",
+    editProfile: "Edit Your Profile",
+  },
+};
