b76f27dc29e27f266fdbf7bac3e6b93d27c9b52d
Mehmet Ali Erfidan <erfidan740@gmail.com>
2026-01-27 21:37:12 +0300

    feat: add ProfilePage

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/common/navbar/Navbar";
+import { Main } from "../styles/GlobalStyles";
+import { mockCurrentUser } from "../data/mockCurrentUser";
+import ProfileHeader from "../components/profile/ProfileHeader";
+import ProfileSummary from "../components/profile/ProfileSummary";
+import ProfileEditForm from "../components/profile/ProfileEditForm";
+import type { UserProfile } from "../config/user.types";
+
+export default function ProfilePage() {
+  const [user, setUser] = useState(mockCurrentUser);
+  const [isEditing, setIsEditing] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSave = (updatedProfile: UserProfile) => {
+    // backend gelince: PUT /me/profile
+    setUser({ ...user, profile: updatedProfile });
+    setIsEditing(false);
+  };
+
+  return (
+    <Main>
+      <Navbar />
+
+      <ProfileHeader user={user} />
+
+      {!isEditing && (
+        <ProfileSummary
+          profile={user.profile}
+          onEdit={() => setIsEditing(true)}
+        />
+      )}
+
+      {isEditing && (
+        <ProfileEditForm
+          initialProfile={user.profile}
+          onCancel={() => setIsEditing(false)}
+          onSave={handleSave}
+        />
+      )}
+
+      <button onClick={() => navigate("/my-notes")}>Notlarım</button>
+    </Main>
+  );
+}
