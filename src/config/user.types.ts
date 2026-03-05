fa27a2ac7629d9a422f3d3dab6a473476f81a8d3
Mehmet Ali Erfidan <211207141+MehmetAliErfidan@users.noreply.github.com>
2026-03-05 23:09:18 +0300

    feat(users): add frontend users types

--- src/config/user.types.ts
+++ src/config/user.types.ts
@@ -1,17 +1,17 @@
 export type UserProfile = {
   fullName?: string | null;
   department?: string | null;
-  grade?: string | number | null;
-  favoriteClass?: string | null;
-  favoriteTeacher?: string | null;
-  favoriteHangoutPlace?: string | null;
-  bio?: string | null;
+  grade?: string | null;
+  favoriteCourse?: string | null;
+  favoriteProfessor?: string | null;
+  favoritePlace?: string | null;
+  about?: string | null;
   avatarUrl?: string | null;
 };
 
 export type CurrentUser = {
   id: number;
-  email: string;
+  email?: string;
   username: string;
   profile: UserProfile | null;
 };
