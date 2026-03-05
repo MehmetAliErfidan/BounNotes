08c3ddbe80cdd5b60a8bcb111cadf2bb91ed0f25
Mehmet Ali Erfidan <211207141+MehmetAliErfidan@users.noreply.github.com>
2026-03-05 23:06:39 +0300

    feat(api): add users router endpoint

--- bounnotes_backend/src/app.ts
+++ bounnotes_backend/src/app.ts
@@ -4,6 +4,7 @@ import notesRouter from "./routes/notes/notes.router";
 import notesAssetsRouter from "./routes/note-assets/note-assets.router";
 import reactionsRouter from "./routes/reactions/reactions.router";
 import authRouter from "./routes/auth/auth.router";
+import usersRouter from "./routes/users/users.router";
 
 const app = express();
 
@@ -21,5 +22,7 @@ app.use("/api/notes", notesRouter);
 app.use("/api/assets", notesAssetsRouter);
 
 app.use("/api/reactions", reactionsRouter);
+
+app.use("/api/users", usersRouter);
 
 export default app;
