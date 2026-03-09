7fb7603d0627d5b982f2a01051053efea6b2cab7
Mehmet Ali Erfidan <211207141+MehmetAliErfidan@users.noreply.github.com>
2026-03-09 16:37:17 +0300

    feat(payments): add iyzico client configuration

---
+++ bounnotes_backend/src/config/iyzico.ts
@@ -0,0 +1,10 @@
+import Iyzipay from "iyzipay";
+import { env } from "./env";
+
+const iyzico = new Iyzipay({
+  apiKey: env.IYZICO_API_KEY,
+  secretKey: env.IYZICO_SECRET_KEY,
+  uri: env.IYZICO_BASE_URL,
+});
+
+export default iyzico;
